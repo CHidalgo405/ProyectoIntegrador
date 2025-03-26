b70a439893628fabc5cc71f202ec1ec9b0b8ad9a
CHidalgo405 <carlosignacio.9836@gmail.com>
2025-03-26 07:43:14 -0600

    Barra de status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StatusBar } from '@capacitor/status-bar';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   showSplash = true;
 
-  constructor() {
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  // Cada vez que se incia la app, se cargan estos valores
+  initializeApp() {
+    this.platform.ready().then(() => {
+      StatusBar.setOverlaysWebView({ overlay: false }); // evita el toolbar se desplace hasta la barra de estado
+      StatusBar.setBackgroundColor({ color: '#4A827E' }); // cambia el color de la barra de estado
+    });
   }
 }
